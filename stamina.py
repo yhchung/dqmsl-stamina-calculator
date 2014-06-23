8ae47cd07f890d56fa9edeb79b639c8847a06e55
Younghyun Chung <younghyun.chung@gmail.com>
2014-06-23 16:37:54 -0400

    starting python code

---
+++ stamina.py
@@ -0,0 +1,15 @@
+# Stamina Calulator
+#
+# 
+
+maxStamina = 64
+dayLightSaving = True
+timeDiff = 13
+staminaPerHour = 6
+
+
+currentStamina = input("Enter current stamina: ")
+neededStamina = input("Enter stamina needed: ")
+#eventTime = input("Enter when it needed: ")
+#input_doubleStamina = input("Double Stamina? (y/n) ")
+
