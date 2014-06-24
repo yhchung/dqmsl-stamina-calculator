f3e29900fe17313ed99bcfe660f9c66ef1b28578
Younghyun Chung <younghyun.chung@gmail.com>
2014-06-24 16:32:30 -0400

    using datetime object

--- stamina.py
+++ stamina.py
@@ -2,16 +2,17 @@
 #
 # 
 import datetime
+from datetime import timedelta
 
 maxStamina = 64
 minutePerStamina = 10
 dayLightSaving = True
 timeDiff = 13
 
-
 currentStamina = input("Enter current stamina: ")
 neededStamina = input("Enter stamina needed: ")
 eventTime = input("Enter when it needed (ex: enter 16 for 4pm): ")
+
 #input_doubleStamina = input("Double Stamina? (y/n) ")
 
 neededTime = (neededStamina - currentStamina) * minutePerStamina
@@ -23,28 +24,23 @@ print "---"
 print str(neededHours) + " Hours " + str(neededMinutes) + " Minutes needed for " + str(neededStamina) + " stamina"
 
 # you can reach the needed stamina at 
+# https://docs.python.org/2/library/datetime.html#strftime-and-strptime-behavior
 now = datetime.datetime.now()
-datetime.time(now.hour, now.minute)
+endTime = now + datetime.timedelta(minutes=neededMinutes, hours=neededHours)
 
-targetHour = now.hour + neededHours
-targetMinute = now.minute + neededMinutes
-
-if targetMinute >= 60 :
-	targetMinute -= 60
-	targetHour += 1
-
-
-print "you can read the needed stamina at " + str(targetHour) + ":" + str(targetMinute)
+print "you can read the needed stamina at " + str(endTime.hour) + ":" + str(endTime.minute)
 
 
 # you can use XX stamina now
-
 eventTimeMinutes = eventTime * 60
 currentTime = now.hour * 60 + now.minute
 remainingTime = eventTimeMinutes - currentTime # minutes
 extraStamina = currentStamina + int(remainingTime/10) - maxStamina
 
-print "you can use " + str(extraStamina) + " stamina now"
+if extraStamina > 0 : 
+	print "you can use " + str(extraStamina) + " stamina now"
+else :
+	print "you can't use stamina now"
 
 
 
